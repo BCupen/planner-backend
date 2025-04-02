87da04e07f47ce1c53e06c94e7a69f62c238ed82
Bry Cupen <bacupen@gmail.com>
2025-04-02 19:16:04 -0400

    feat: get all todos

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,22 +3,42 @@ import Todo from "../models/todo.model.js";
 
 const todoRouter = express.Router();
 
+// POST new todo
 todoRouter.post("/", async (req, res) => {
-    const todo = req.body;
+  const todo = req.body;
 
-    if(!todo.title || !todo.description || !todo.priority || !todo.dueDate){
-        return res.status(400).json({ success: false, message: "Missing fields"});
-    }
+  if (!todo.title || !todo.description || !todo.priority || !todo.dueDate) {
+    return res.status(400).json({ success: false, message: "Missing fields" });
+  }
 
-    const newTodo = new Todo(todo);
+  const newTodo = new Todo(todo);
 
-    try{
-        await newTodo.save();
-        return res.status(201).json({success:true, message: "Todo created", data: newTodo});
-    } catch(error) {
-        console.log(`Error saving todo: ${error.message}`);
-        return res.status(500).json({success: false, message: "Error saving todo"});
-    }
-})
+  try {
+    await newTodo.save();
+    return res
+      .status(201)
+      .json({ success: true, message: "Todo created", data: newTodo });
+  } catch (error) {
+    console.log(`Error saving todo: ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error saving todo" });
+  }
+});
+
+// GET all todos
+todoRouter.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    console.log(`Todos: ${todos}`);
+    return res.status(200).json(todos);
+  } catch (error) {
+    console.log(`Error fetching todos: ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      message: "Error fetching todos",
+    });
+  }
+});
 
 export default todoRouter;
