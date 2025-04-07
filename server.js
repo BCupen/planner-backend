6e05ca55c2d086fde12bf989a36bc76abf418ab3
Bry Cupen <bacupen@gmail.com>
2025-04-07 18:38:03 -0400

    feat: add cors

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import todoRouter from "./routes/todo.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/todos", todoRouter);
 
 app.listen(8686, () => {
-    connectDB();
-    console.log("Server has been started");
-})
+  connectDB();
+  console.log("Server has been started");
+});
